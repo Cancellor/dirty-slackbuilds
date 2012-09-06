8fe8020e8c108535bfc49d07c78f361a7ef5fc4f
Fridrich von Stauffenberg <cancellor2@gmail.com>
2012-09-07 00:14:03 +0300

    00-alternate/openmotif: Fixed doinst.sh for 64-bit systems

--- 00-alternate/openmotif/doinst.sh
+++ 00-alternate/openmotif/doinst.sh
@@ -1,5 +1,9 @@
+if [ $(uname -m) = x86_64 ]; then
+  LIBDIRSUFFIX=64
+fi
+
 if ! grep /usr/dt/lib etc/ld.so.conf > /dev/null 2>&1; then
-  echo "/usr/dt/lib" >> etc/ld.so.conf
+  echo "/usr/dt/lib$LIBDIRSUFFIX" >> etc/ld.so.conf
 fi
 
 if [ -x /sbin/ldconfig ]; then
